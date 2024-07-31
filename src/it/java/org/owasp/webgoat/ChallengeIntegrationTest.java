809b49b03e884fe5e65bc94be177bf1907dc7d64
sko <sko@ezcure.co.kr>
2024-07-31 16:08:01 +0900

    commit

--- src/it/java/org/owasp/webgoat/ChallengeIntegrationTest.java
+++ src/it/java/org/owasp/webgoat/ChallengeIntegrationTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 // import org.springframework.http.HttpStatus;
 
 public class ChallengeIntegrationTest extends IntegrationTest {
