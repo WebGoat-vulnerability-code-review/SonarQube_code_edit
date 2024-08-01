22c3b908a165833a436025e3c73c4162ca846332
sko <sko@ezcure.co.kr>
2024-08-01 11:31:52 +0900

    commit

--- src/test/java/org/owasp/webgoat/lessons/sqlinjection/mitigation/SqlOnlyInputValidationOnKeywordsTest.java
+++ src/test/java/org/owasp/webgoat/lessons/sqlinjection/mitigation/SqlOnlyInputValidationOnKeywordsTest.java
@@ -14,33 +14,32 @@ public class SqlOnlyInputValidationOnKeywordsTest extends SqlLessonTest {
   @Test
   public void solve() throws Exception {
     mockMvc
-        .perform(
-            MockMvcRequestBuilders.post("/SqlOnlyInputValidationOnKeywords/attack")
-                .param(
-                    "userid_sql_only_input_validation_on_keywords",
-                    "Smith';SESELECTLECT/**/*/**/FRFROMOM/**/user_system_data;--"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.lessonCompleted", is(true)))
-        .andExpect(jsonPath("$.feedback", containsString("passW0rD")));
+            .perform(
+                    MockMvcRequestBuilders.post("/SqlOnlyInputValidationOnKeywords/attack")
+                            .param(
+                                    "userid_sql_only_input_validation_on_keywords",
+                                    "Smith';SESELECTLECT/**/*/**/FRFROMOM/**/user_system_data;--"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.lessonCompleted", is(true)))
+            .andExpect(jsonPath("$.feedback", containsString("passW0rD")));
   }
 
   @Test
   public void containsForbiddenSqlKeyword() throws Exception {
     mockMvc
-        .perform(
-            MockMvcRequestBuilders.post("/SqlOnlyInputValidationOnKeywords/attack")
-                .param(
-                    "userid_sql_only_input_validation_on_keywords",
-                    "Smith';SELECT/**/*/**/from/**/user_system_data;--"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.lessonCompleted", is(false)))
-        .andExpect(
-           jsonPath(
-              "$.output",
-              containsString(
-                  "unexpected token: *<br> Your query was: SELECT * FROM user_data WHERE"
-                      + " last_name ="
-                      + " 'SMITH';\\\\\\/**\\\\\\/*\\\\\\/**\\\\\\/\\\\\\/**\\\\\\/USER_SYSTEM_DATA;--'")));
+            .perform(
+                    MockMvcRequestBuilders.post("/SqlOnlyInputValidationOnKeywords/attack")
+                            .param(
+                                    "userid_sql_only_input_validation_on_keywords",
+                                "Smith';SELECT/**/*/**/from/**/user_system_data;--"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.lessonCompleted", is(false)))
+            .andExpect(
+                    jsonPath(
+                            "$.output",
+                            containsString(
+                                    "unexpected token: *<br> Your query was: SELECT * FROM user_data WHERE"
+                                            + " last_name ="
+                                            + " 'SMITH';\\\\\\/**\\\\\\/*\\\\\\/**\\\\\\/\\\\\\/**\\\\\\/USER_SYSTEM_DATA;--'")));
   }
-
 }
